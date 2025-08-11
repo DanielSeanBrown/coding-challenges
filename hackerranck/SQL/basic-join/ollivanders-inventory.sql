00035062527ce92a940cd22cc2b81ce06e8bb0b2
DanielSeanBrown <danielseanbrown@gmail.com>
2025-08-11 21:28:35 +0100

    Update ollivanders-inventory.sql

--- hackerranck/SQL/basic-join/ollivanders-inventory.sql
+++ hackerranck/SQL/basic-join/ollivanders-inventory.sql
@@ -1,1 +1,11 @@
-
+SELECT W.id, WP.age, W.coins_needed, W.power
+FROM Wands W
+INNER JOIN Wands_Property WP
+ON W.code = WP.code
+WHERE WP.is_evil = 0 
+    AND W.coins_needed = (SELECT MIN(V.coins_needed)
+                              FROM Wands V
+                              INNER JOIN Wands_Property VP
+                              ON V.code = VP.code
+                              WHERE V.power = W.power AND VP.age = WP.age) 
+ORDER BY W.power DESC, WP.age DESC;
